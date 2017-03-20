bb6b0e3831f74b521f78ed901409aa6c2f84f1d3
Mario Duarte <djtsunamimario@gmail.com>
2017-03-20 00:55:53 +0000

    added single video and list videos - added pagination and finalized styles

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	{{#each items}}
 	<div class="video-container">
 		<div class="video-content">
-			<a href="#0" data-id="{{snippet.resourceId.videoId}}">
+			<a class="js-open-video" href="#0" data-id="{{snippet.resourceId.videoId}}">
 				<h2>{{snippet.title}}</h2>
 			</a>
 			<h4>Published on {{formatDate snippet.publishedAt}}</h4>
@@ -23,11 +23,32 @@
 		</div>
 
 		<div class="video-thumbnail">
-			<a href="#0" data-id="{{snippet.resourceId.videoId}}">
+			<a class="js-open-video" href="#0" data-id="{{snippet.resourceId.videoId}}">
 				<img src="{{snippet.thumbnails.medium.url}}" srcset="{{snippet.thumbnails.standard.url}} 768w, {{snippet.thumbnails.high.url}} 481w" sizes="(min-width: 481px) 768px, 100vw" alt="{{snippet.title}}">
 			</a>
 		</div>
 	</div>
+	{{/each}}
+	{{#if nextPageToken}}
+		<a href="#0" class="btn js-nex-page" data-token="{{nextPageToken}}">Next Page</a>
+	{{/if}}
+</script>
+
+<script id="single-video-item" type="text/x-handlebars-template">
+	{{#each items}}
+		<a href="#0" class="js-return-list"><i class="fa fa-angle-left" aria-hidden="true"></i>Back to list of videos</a>
+		<h2>{{snippet.title}}</h2>
+		<h4>Published on {{formatDate snippet.publishedAt}}</h4>
+
+		<div class="video-item-container">
+			<div class="video">
+				<iframe width="auto" height="auto" src="https://www.youtube.com/embed/{{id}}" frameborder="0" allowfullscreen style="width:100%;"></iframe>
+			</div>
+			<div class="video-description">
+				<p>{{snippet.description}}</p>
+			</div>
+		</div>
+
 	{{/each}}
 </script>
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
 
-	const url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails,status&maxResults=10&playlistId=PL2Ouhqcw7R4u1Aiou9Y2AabpVHVxp-T_i&key=AIzaSyCuv_16onZRx3qHDStC-FUp__A6si-fStw";
-	let source   = $("#video_list_item").html();
-	let template = Handlebars.compile(source);
-	let html = null;
+	const key = "AIzaSyCuv_16onZRx3qHDStC-FUp__A6si-fStw";
+	const url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails,status&maxResults=10&playlistId=PL2Ouhqcw7R4u1Aiou9Y2AabpVHVxp-T_i&key="+ key;
+	let sourceList   = $("#video_list_item").html();
+	let sourceItem = $('#single-video-item').html();
+	let templateList = Handlebars.compile(sourceList);
+	let templateItem = Handlebars.compile(sourceItem);
+	let htmlList = null;
+	let htmlItem = null;
 
 	$.getJSON( url, function( data ) {
-		let container = $('.js-list-container');
-		html = template(data);
-		console.log(html);
-		container.html(html);
+		htmlList = templateList(data);
+		$(htmlList).appendTo('.js-list-container');
 	});
 
 	Handlebars.registerHelper ('truncate', function (str, len) {
@@ -24,9 +26,38 @@ $(document).ready(function(){
 	});
 
 	Handlebars.registerHelper('formatDate', function(date){
-		let months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+		let months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
 		let newDate = new Date(date);
 		return months[newDate.getMonth()] + ' ' + newDate.getDate() + ',' + newDate.getFullYear();
+	});
+
+	$('body').on('click', '.js-open-video', function(e){
+		e.preventDefault();
+		let itemContainer = $('.js-item-container');
+		let vidID = $(this).data('id');
+		let videoURL = "https://www.googleapis.com/youtube/v3/videos?part=id%2Csnippet&id="+ vidID +"&key="+ key;
+		$.getJSON( videoURL, function( data ) {
+			htmlItem = templateItem(data);
+			console.log(data);
+			itemContainer.html(htmlItem).addClass('open');
+		});
+	});
+
+	$('body').on('click', '.js-return-list', function(e){
+		e.preventDefault();
+		$('.js-item-container').removeClass('open').html('');
+	});
+
+	$('body').on('click', '.js-nex-page', function(e){
+		e.preventDefault();
+		let token = $(this).data('token');
+		let nextPage = url + "&pageToken=" + token;
+		$(this).hide(500).remove();
+		$.getJSON( nextPage, function( data ) {
+			console.log(data);
+			htmlList = templateList(data);
+			$(htmlList).appendTo('.js-list-container');
+		});
 	});
 
 });
